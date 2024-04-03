d8b22916cbfa290744f00144d58920f28bb11f94
OJddJO <jeremyli2906@hotmail.com>
2024-04-03 08:56:23 +0200

    a

--- interpreter.c
+++ interpreter.c
@@ -4,18 +4,33 @@
 #include "list.h"
 #include "piles.h"
 
+int execute_line(char *line, struct pile *p) {
+    
+}
+
 int main(void) {
     char code[] = "";
-    struct list lines;
-    init_list(&lines);
-    size_t codeLen = strlen(code);
-    for (size_t i = 0; i < codeLen; i++) {
+    size_t code_len = strlen(code);
+    int num_lines = 0;
+    for (size_t i = 0; i < code_len; i++) {
         if (code[i] == '\n') {
-            
-            add_element(&lines, i);
+            num_lines++;
+        }
+    }
+    char lines[num_lines][250];
+    int lineIndex = 0;
+    int charIndex = 0;
+    // Split code into lines
+    for (size_t i = 0; i < code_len; i++) {
+        if (code[i] == '\n') {
+            lines[lineIndex][charIndex] = '\0';
+            lineIndex++;
+            charIndex = 0;
+        } else {
+            lines[lineIndex][charIndex] = code[i];
+            charIndex++;
         }
     }
     struct pile p;
     init_pile(&p);
-    
 }
